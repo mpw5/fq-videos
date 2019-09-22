eb466b9e2fe53f970d663500aed39c2e16aeeacd
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2019-09-22 20:45:48 +0100

    ajax call

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const moment = require('moment');
+const fs = require('fs');
+const schedule = require('node-schedule');
 
 const {
   WebClient
@@ -76,11 +79,11 @@ async function get_results() {
 
             if (typeof(video_html) != 'undefined') {
               let video = {
-                "username": '@' + result['username'],
-                "date": new Date(result['ts'] * 1000),
-                "title": attachment['title'],
-                "title_link": attachment['title_link'],
-                "video_html": video_html.replace("autoplay=1", "autoplay=0&rel=0")
+                username: '@' + result['username'],
+                date: moment(new Date(result['ts'] * 1000)).format('YYYY/MM/DD HH:mm:ss'),
+                title: attachment['title'],
+                title_link: attachment['title_link'],
+                video_html: video_html.replace("autoplay=1", "autoplay=0&rel=0")
               }
 
               results = results.concat(video);
@@ -92,14 +95,16 @@ async function get_results() {
   }
 
   sorted_results = results.sort(compareValues('date'));
+
+  fs.writeFile("results.txt", JSON.stringify(sorted_results), function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
 }
 
 router.get('/', (req, res) => {
-<<<<<<< HEAD
-  if(typeof(sorted_results)){
-=======
   if(typeof(sorted_results) == 'undefined'){
->>>>>>> a09c9b1... add scheduler for api calls
     res.render('error');
   } else {
     res.render('index', {
@@ -108,13 +113,18 @@ router.get('/', (req, res) => {
   }
 });
 
-var schedule = require('node-schedule');
+router.get('/results', (req, res) => {
+  fs.readFile('results.txt', 'utf8', function(err, data) {
+    if (err) {
+      console.log(err);
+    }else{
+        console.log('reading results.txt')
+        res.send({data: JSON.parse(data)});
+    }
+  });
+});
 
-<<<<<<< HEAD
 schedule.scheduleJob('*/5 * * * *', function(){
-=======
-schedule.scheduleJob('*/2 * * * *', function(){
->>>>>>> a09c9b1... add scheduler for api calls
   get_results()
 });
 
